6744d689274f1a3b08dc26c783b4706c1498b386
Michael Li <dev.mike011235@gmail.com>
2020-08-01 23:27:28 -0400

    implemented constructor and adders for the company object in queries.js file.

---
+++ master/db/db_queries.js
@@ -0,0 +1,25 @@
+const connection = require("./connection");
+
+class Company {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+
+    // Add employees, roles, and departments.
+    addEmployee(employee) {
+        return this.connection.query("INSERT INTO employee SET ?", employee);
+    }
+
+    addRole(role) {
+        return this.connection.query("INSERT INTO role SET ?", role);
+    }
+
+    addDepartment(department) {
+        return this.connection.query("INSERT INTO department SET ?", department);
+    }
+
+    
+}
+
+module.exports = new Company(connection);
